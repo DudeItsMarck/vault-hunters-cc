1393898fe5f1c4f0a14cfba4fcf705d11b20c2d1
DudeItsMarck <marcklaurent@live.dk>
2022-02-07 09:30:21 +0100

    .

--- Mining.lua
+++ Mining.lua
@@ -2,27 +2,40 @@ local args = {...}
 
 local torchPlaceFrequency = 12
 local torchSlot = 1
-local distanceFromLastTorch = 0
+local distanceFromLastTorch = 12
 local torchName = "minecraft:torch"
 local distance = args[1]
 
-function torchSlot()
+function TorchSlot()
     for i=1,16 do
         turtle.select(i)
         local data = turtle.getItemDetail(i)
         if data then
-            -- print(data.name)
             if data.name == torchName then
                 torchSlot = i
-                print(torchSlot)
                 return
             end
         end
     end
 end
 
-function main()
-    torchSlot()
+function DoMine()
+    for i=1,distance do
+        if distanceFromLastTorch == torchPlaceFrequency then
+            turtle.select(torchSlot)
+            turtle.placeDown()
+            distanceFromLastTorch = 0
+        end
+        turtle.dig()
+        turtle.forward()
+        turtle.digUp()
+        distanceFromLastTorch = distanceFromLastTorch + 1
+    end
 end
 
-main()
+function Main()
+    TorchSlot()
+    DoMine()
+end
+
+Main()
