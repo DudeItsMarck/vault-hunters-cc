58b64d0db8dc5823fa9fe07c9088d795014928c7
DudeItsMarck <marcklaurent@live.dk>
2022-02-07 08:57:05 +0100

    .

--- Mining.lua
+++ Mining.lua
@@ -14,7 +14,7 @@ function doStuff()
             print(turtle.getItemDetail(i).name)
             if data.name == torchName and i == not torchSlot then
                 torchSlot = i
-                break
+                break end
             end
         end
     end
