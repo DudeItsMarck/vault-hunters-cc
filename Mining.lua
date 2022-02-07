23021ce8a97c4b389c6635cf15fcc5e13c0e9b57
DudeItsMarck <marcklaurent@live.dk>
2022-02-07 08:30:00 +0100

    .

--- Mining.lua
+++ Mining.lua
@@ -3,4 +3,14 @@ local args = {...}
 local torchPlaceFrequency = 12
 local distance = args[1]
 
-print(distance)
+for i=1,distance do
+    
+end
+
+function doStuff()
+    for i=1,16 do
+        turtle.select(i)
+        -- if turtle.getItemDetail(i)
+        print(turtle.getItemDetail(i))
+    end
+end
